06256cc735f4e05caddedb52dd92f3398df837e9
Jeferson de Oliveira <jheferson.contato@hotmail.com>
2023-04-20 04:30:17 -0300

    correção do parallax effect

--- src/Components/Paralax/index.jsx
+++ src/Components/Paralax/index.jsx
@@ -8,8 +8,13 @@ export const ParallaxComponent = () => {
 
   useEffect(() => {
     const handleScroll = () => {
-      setValue(window.scrollY);
+      if (window.scrollY < 1300) {
+        setValue(window.scrollY);
+      } else {
+        setValue(0);
+      }
     };
+
     window.addEventListener('scroll', handleScroll);
     return () => {
       window.removeEventListener('scroll', handleScroll);
@@ -21,18 +26,28 @@ export const ParallaxComponent = () => {
       <div className="flex justify-center	h-screen relative object-cover">
         <div style={{ marginTop: value * 0.5 + 'px' }}>
           <Image
-            className=" t-0	l-0 r-0 b-0 h-fit	 w-full pointer-events-none opacity-20	z-50 "
+            className=" t-0	l-0 r-0 b-0 	w-full pointer-events-none opacity-20 object-cover"
             src={imgBackground}
-            width={1340}
-            height={1340}
+            width={3000}
+            height={3000}
           />
         </div>
-        <div className="text mt-16 absolute " style={{ marginTop: value * 1.5 + 'px' }}>
+        <div className="text-center mt-16 absolute " style={{ bottom: value * 1.5 + 'px' }}>
+          <h1 className="mt-18 font-mono text-white text-5xl">
+            ACADEMIA DE ARTES MARCIAIS BUSHIDO
+          </h1>
+          <h2 className="mt-10 font-mono text-white text-3xl">
+            Na Academia de Judô Bushido, você é desafiado a superar seus limites e medos para
+            alcançar a excelência, tanto dentro quanto fora do tatame
+          </h2>
+        </div>
+        <div className="mt-16 absolute " style={{ top: value * 1.0 + 'px' }}>
           <Image
-            className="relative t-0	l-0 w-full pointer-events-none	"
+            className="relative mt-12	l-0 w-full pointer-events-none	"
             src={img}
             width={600}
             height={600}
+            // style={({ width: value * 0.6 + 'px' }, { height: value * 0.6 + 'px' })}
           />
         </div>
       </div>
--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -1,9 +1,15 @@
 import { Html, Head, Main, NextScript } from 'next/document';
+import Link from 'next/link';
 
 export default function Document() {
   return (
     <Html lang="pt-BR">
-      <Head />
+      <Head>
+        <Link
+          href="https://fonts.googleapis.com/css2?family=Teko:wght@300;400;500;600;700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
