fc33638b544b66f37aa1c885cbd5f0f90b00dfc1
Jeferson Oliveira <adv.jefersonoliveira@gmail.com>
2024-02-20 17:13:55 -0300

    Fix(professores): Correção de cards dos professores

--- src/Components/TeacherCard/index.jsx
+++ src/Components/TeacherCard/index.jsx
@@ -1,7 +1,7 @@
 import { teacherData } from './teacherData';
 export const TeacherCard = () => {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4">
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-4 gap-4">
       {teacherData.map((data, index) => {
         return (
           <div
